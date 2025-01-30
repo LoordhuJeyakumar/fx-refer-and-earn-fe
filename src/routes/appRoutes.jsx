3245d1f9d9a8fc19ce51b9ea5d483bed282794d9
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2025-01-30 16:10:30 +0530

    add routing for sign in, sign up, and contact pages; include footer and back to top components

--- src/routes/appRoutes.jsx
+++ src/routes/appRoutes.jsx
@@ -1,13 +1,30 @@
-import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import ReferAndEarnLandingPage from "../../pages/ReferAndEarnLandingPage";
+import SignUp from "../components/SignUp";
+import SignIn from "../components/SignIn";
+import NotFound from "../components/NotFound";
+import ContactUs from "../components/ContactUs";
+import Footer from "../components/Footer";
+import BackToTop from "../components/BackToTop";
+import ReferAndEarnLandingPage from "../pages/ReferAndEarnLandingPage";
 
 function appRoutes() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<ReferAndEarnLandingPage />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Routes>
+          <Route path="/" element={<ReferAndEarnLandingPage />} />
+          <Route path="/about" element={<NotFound />} />
+          <Route path="/contact" element={<NotFound />} />
+          <Route path="/resources" element={<NotFound />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <BackToTop />
+        <Footer />
+      </div>
     </Router>
   );
 }
